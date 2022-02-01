415e26ceb20e43626d4185548f5147ba650e370a
mvasilei <53435274+mvasilei@users.noreply.github.com>
2022-02-01 08:57:36 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+version = '17.03.03' #Must be exactly as appeared in show ver
+rollback_version = 'V169_1A_ES04' #Must be exactly as appeared in show ver
+ios_binary = 'asr920igp-universalk9_npe.17.03.03.SPA.bin' #binary file for target ios version
+rommon_binary = 'asr920igp-15_6_43r_s_rommon.pkg' #file used to upgrade rommon if upgrade not required rommon_binary = ''
+rollback_binary = 'asr920igp-universalk9.V169_1A_ES04.SPA.bin' #binary file to use in case of rollback
